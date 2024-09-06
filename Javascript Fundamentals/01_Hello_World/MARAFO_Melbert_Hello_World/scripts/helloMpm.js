d82fb18585ddb2d8ab54b4106a6a846266aca364
codexcancerion <melbertmarafo2022@gmail.com>
2024-09-06 17:16:59 +0800

    Updated changing background color

--- Javascript Fundamentals/01_Hello_World/MARAFO_Melbert_Hello_World/scripts/helloMpm.js
+++ Javascript Fundamentals/01_Hello_World/MARAFO_Melbert_Hello_World/scripts/helloMpm.js
@@ -3,3 +3,10 @@ document.getElementById("nameMpm").innerHTML = nym;
 
 const color = prompt("What is your preferred background color?");
 document.getElementById("nameMpm").style.color = color;
+
+
+setInterval(() => {
+    let n = (Math.random() * 0xffff * 10000).toString(16);
+    n = '#'+ n.slice(0, 6);
+    document.body.style.backgroundColor = n;
+}, 1000)
