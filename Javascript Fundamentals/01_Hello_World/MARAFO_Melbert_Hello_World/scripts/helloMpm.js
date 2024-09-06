5122f2f8169312e1f76e9dcc23544351170b6855
codexcancerion <melbertmarafo2022@gmail.com>
2024-09-06 17:13:34 +0800

    Updated js on external

---
+++ Javascript Fundamentals/01_Hello_World/MARAFO_Melbert_Hello_World/scripts/helloMpm.js
@@ -0,0 +1,5 @@
+const nym = prompt("What is your name?");
+document.getElementById("nameMpm").innerHTML = nym;
+
+const color = prompt("What is your preferred background color?");
+document.getElementById("nameMpm").style.color = color;
