e4694e4d5500a0704cb2bf8f04e4f62e393afe4a
job <jobpongdad@gmail.com>
2024-09-20 18:15:27 +0800

    created dice and fix index

---
+++ javascript_fundamentals/02_Roll_The_Dice/Pongdad_Job_Roll_The_Dice/scripts/dice.js
@@ -0,0 +1,18 @@
+const rollBtn = document.getElementById("rollBtn");
+
+const roll = () => {
+    const dimension = 116;
+    const die1 = Math.floor(Math.random() * 6) + 1;
+    const die1Element = document.getElementById("kevin");
+    console.log(die1);
+
+
+    die1Element.style.backgroundPositionX = `-${dimension * (die1 - 1)}px`;
+    die1Element.style.backgroundPositionY = `-${die1 >= 3 ? dimension * 1 : 0}px`;
+
+
+};
+
+rollBtn.onclick = () => {
+    roll();
+};
