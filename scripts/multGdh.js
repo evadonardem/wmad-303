9b6721856b00c1312201cbe125ffffaa77ec5da2
Bacasen,John <2023-01-0841@kcp.edu.ph>
2024-09-28 17:15:12 +0800

    hig

---
+++ scripts/multGdh.js
@@ -0,0 +1,35 @@
+const awesomeMultTableFormKdn = document.querySelector('#awesomeMultTableFormKdn');
+
+awesomeMultTableFormKdn.onsubmit = (event) => {
+    event.preventDefault();
+    const rows = document.querySelector('input[name="rows"]').value;
+    const cols = document.querySelector('input[name="columns"]').value;
+
+
+
+    const multCols = document.getElementById('multCols');
+    let colsHeader = '<th></th>';
+    for (let i = 1; i <= cols; i++) {
+        colsHeader += `<th>${i}</th>`;
+    }
+    multCols.innerHTML = colsHeader;
+
+
+    const multRows = document.getElementById('multRows');
+    let tableRows = '';
+
+
+    for (let i = 1; i <= rows; i++) {
+        tableRows += '<tr>';
+        tableRows += `<th scope="rows">${i}</th>`
+        for (let j = 1; j <= cols; j++) {
+
+            tableRows += `<td>${i * j}</td>`;
+        }
+
+        tableRows += '</tr>'
+
+    }
+    multRows.innerHTML = tableRows;
+
+};
