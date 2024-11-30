59c8e3f0fba738befabd394000076d780aea81bb
ClintKycer <basilioclint@gmail.com>
2024-11-30 18:06:27 +0800

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+});
