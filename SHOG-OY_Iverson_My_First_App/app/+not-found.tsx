99478164118f881e8829e5d90728a3cc8d83e878
“Iver” <iver@gmail.com>
2024-12-02 10:19:44 +0800

    dondone

--- SHOG-OY_Iverson_My_First_App/app/+not-found.tsx
+++ SHOG-OY_Iverson_My_First_App/app/+not-found.tsx
@@ -4,7 +4,7 @@ import { Link, Stack } from 'expo-router';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+      <Stack.Screen options={{ title: 'Oops! WRONG WAY!' }} />
       <View style={styles.container}>
         <Link href="/" style={styles.button}>
           Go back to Home screen!
@@ -17,7 +17,7 @@ export default function NotFoundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
   },
--- SHOG-OY_Iverson_My_First_App/app/_layout.tsx
+++ SHOG-OY_Iverson_My_First_App/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)IS" options={{ headerShown: false }} />
       <Stack.Screen name="+not-foundIS" />
     </Stack>
   );
