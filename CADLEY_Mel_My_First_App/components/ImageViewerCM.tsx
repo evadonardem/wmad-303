0a81e2452683a5fea375599b73ee1699f3422e50
melkhadlee147 <melkhadlee147@gmail.com>
2024-12-06 17:28:52 +0800

    agbalin kan ah pogi

---
+++ CADLEY_Mel_My_First_App/components/ImageViewerCM.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { Image, type ImageSource } from "expo-image";
+
+type Props = {
+  imgSource: ImageSource;
+  selectedImage?: string;
+};
+
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
