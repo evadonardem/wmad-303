981328bad2bfd598d4df90440b5a20a7ab0f7833
Kevin <nacz.kevz13@gmail.com>
2024-12-18 14:36:20 +0800

    awesome_table

---
+++ 04_javascript_fundamentals/KIS-ING_JOE_AWESOME_MULT_TABLE/scripts/multKJ.js
@@ -0,0 +1,34 @@
+const awesomeMultTableFormIS = 
+document.querySelector('#awesomeMultTableFormkj');
+
+awesomeMultTableFormIS.onsubmit = (event) => {
+    event.preventDefault();
+    
+    const rows = document.querySelector('input[name="rows"]').value;
+    const cols = document.querySelector('input[name="columns"]').value;
+
+    const multCols = document.getElementById('multCols');
+    let colsHeader = '<th></th>';
+    for (let i = 1; i <= cols; i++) {
+        colsHeader += `<th> ${i} </th>`;
+    }
+    multCols.innerHTML = colsHeader;
+
+    const multRows = document.getElementById('multRows');
+    let tableRows ='';
+    for (let i = 1; i < rows; i++){
+        tableRows += '<tr>';
+        tableRows += `<th scope="rows"> ${i} </th>`
+        for (let j = 1; j <= cols; j++){
+            const prod = i * j;
+            const isEven = prod % 2 === 0 ? 'success' : 'danger';
+            tableRows += `<td> 
+                <p class="alert alert-${isEven}">
+                    ${prod}
+                </p>
+            </td>`;
+        }
+        tableRows += '</tr>';
+    }
+    multRows.innerHTML = tableRows;
+}
