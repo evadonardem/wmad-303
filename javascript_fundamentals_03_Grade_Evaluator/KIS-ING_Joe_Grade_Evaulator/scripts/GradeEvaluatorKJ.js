7d53186b9127943f23a0c5bfa6073493eb75c661
Kevin <nacz.kevz13@gmail.com>
2024-12-18 14:27:22 +0800

    grade evaluator

---
+++ javascript_fundamentals_03_Grade_Evaluator/KIS-ING_Joe_Grade_Evaulator/scripts/GradeEvaluatorKJ.js
@@ -0,0 +1,39 @@
+const gradeElement = document.getElementById("yourGradekj");
+const gradeCard = document.getElementById("gradekj");
+const cardHeader = document.getElementById("cardHeader");
+const cardTitle = document.getElementById("cardTitle");
+const cardText = document.getElementById("cardText");
+
+const gradeEvaluator = (grade) => {
+    if (grade >= 95 && grade <= 100) {
+        cardHeader.textContent = "Excellent";
+        cardTitle.textContent = "Passed";
+        cardText.textContent = "KAlalaingan kan unay apo!";
+        gradeCard.className = "card border-success mb-5";
+    } else if (grade >= 90) {
+        cardHeader.textContent = "Great Job";
+        cardTitle.textContent = "Passed";
+        cardText.textContent = "Nalaing naka pasa kan!";
+        gradeCard.className = "card border-success mb-5";
+    } else if (grade >= 75) {
+        cardHeader.textContent = "Passed";
+        cardTitle.textContent = "Good effort";
+        cardText.textContent = "Keep improving!";
+        gradeCard.className = "card border-warning text-bg-warning mb-5";
+    } else if (grade < 75 && grade >= 0) {
+        cardHeader.textContent = "Failed";
+        cardTitle.textContent = "Needs Improvement";
+        cardText.textContent = "Don't give up! Try again.";
+        gradeCard.className = "card text-bg-dark mb-5";
+    } else {
+        cardHeader.textContent = "Invalid Grade";
+        cardTitle.textContent = "";
+        cardText.textContent = "Please enter a valid grade between 0 and 100.";
+        gradeCard.className = "card border-secondary text-bg-danger mb-5";
+    }
+};
+
+gradeElement.onchange = (event) => {
+    const grade = parseFloat(event.target.value);
+    gradeEvaluator(grade);
+};
